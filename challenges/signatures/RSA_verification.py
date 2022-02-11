682f045efc821b7ede685c288a6e64d45866888e
Julien Bocage <julien.bocage@gmail.com>
2022-02-11 16:47:08 +0100

    Finished RSA signature checking

--- challenges/signatures/RSA_verification.py
+++ challenges/signatures/RSA_verification.py
@@ -23,8 +23,24 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.exceptions import InvalidSignature
 
 server_pub_key_repr = parse.parse('{}: "{}"', instructions[1])[1]
-serv_pub_key = load_pem_public_key(server_pub_key_repr)
+serv_pub_key = load_pem_public_key(bytes.fromhex(server_pub_key_repr))
 
 
 for i in range(30):
+    question = r.readline().decode()
+    parsed = parse.parse('{}"{}"{}"{}"', question.strip())
+    text = parsed[1].encode()
+    signature = bytes.fromhex(parsed[3])
+    pad = padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH)
+    hasher = hashes.SHA256()
+    try:
+        verif = serv_pub_key.verify(signature,text,pad,hasher)
+        r.sendline('true')
+    except:
+        r.sendline('false')
+    # print(r.readline())
     pass
+
+print(r.recvall())
